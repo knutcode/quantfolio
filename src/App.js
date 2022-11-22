d051301d0f7073d2825c8503501de9bda613b145
knutc <knutc.kodehode@gmail.com>
2022-11-22 13:44:33 +0100

    wrapped app content in a flex container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { HighChartWrapper } from './components/chart/Chart';
+
 import { Navbar } from './components/navbar/Navbar';
 
 function App() {
 	return (
-		<>
+		<section>
 			<Navbar />
 			<HighChartWrapper />
-		</>
+		</section>
 	);
 }
 
